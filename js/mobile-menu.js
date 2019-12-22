4336e391ed3597c715bd0e24e9bce546572142e7
Roman <pomankrentovsky@gmail.com>
2019-12-22 19:29:07 +0200

    Сделал 1 экран

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -3,15 +3,25 @@
         mobileMenu = document.querySelector('.header__nav');
   let count = 0;
 
-  mobileMenuIcon.addEventListener('click', function() {
+  function showMenu() {
     if(count === 1) {
       mobileMenu.style = `left: -1000px`;
       count = 0;
     }
     else {
-      mobileMenu.style = `left: 0px`;
+      mobileMenu.style = `left: -46px`;
       count++;
     }
     mobileMenuIcon.classList.toggle('header-mobile-2');
-  });
+  };
+
+  function showMenu2(evt) {
+    if(evt.key === 'Enter') {
+      showMenu();
+    }
+  }
+
+  mobileMenuIcon.addEventListener('click', showMenu);
+  mobileMenuIcon.addEventListener('keydown', showMenu2);
+
 })();
