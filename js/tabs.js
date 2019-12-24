05b787f715ddbb0087a7df258db86728a39a257a
Roman <pomankrentovsky@gmail.com>
2019-12-24 07:39:43 +0200

    Сделал поддержку клавиатуры(второй экран)

--- js/tabs.js
+++ js/tabs.js
@@ -1,10 +1,18 @@
 (function() {
   const body = document.querySelector('.secons-screen__body'),
         tabs = document.querySelectorAll('.second-screen__left__item');
+  function showTabs(evt) {
+    body.style.top = `-${evt.target.dataset.value}%`;
+  }
+
+  function showTabs2(evt) {
+    if(evt.key === 'Enter') {
+      body.style.top = `-${evt.target.dataset.value}%`;
+    }
+  }
 
   for (let i = 0; i < tabs.length; i++) {
-    tabs[i].addEventListener('click', function(evt) {
-      body.style.top = `-${evt.target.dataset.value}%`;
-    }) 
+    tabs[i].addEventListener('click', showTabs);
+    tabs[i].addEventListener('keydown', showTabs2);
   }
 })();
