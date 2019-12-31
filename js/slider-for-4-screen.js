dd3012a4484ce75d42660a3df5fea921e4046df0
Roman <pomankrentovsky@gmail.com>
2019-12-31 09:32:26 +0200

    Сделал адаптацию под мобилки(4 экран)

--- js/slider-for-4-screen.js
+++ js/slider-for-4-screen.js
@@ -5,23 +5,83 @@
   
   function createSlider(left, right, content) {
     let count = 0;
-    left.addEventListener('click', function() {
-      if(count === 0) {
-        count = -108;
-      } else {
-        count+=36;
-      }
-      content.style.left = `${count}%`;
-    });
+    const width  = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 
-    right.addEventListener('click', function() {
-      if(count === -108) {
-        count = 0;
-      } else {
-        count-=36;
+    if(width <= 1250 && width > 1170) {
+      function moveLeft() {
+        if(count === 0) {
+          count = -208;
+        } else {
+          count+=52;
+        }
+        content.style.left = `${count}%`;
       }
-      content.style.left = `${count}%`;
-    });
+  
+      function moveRight() {
+        if(count === -208) {
+          count = 0;
+        } else {
+          count-=52;
+        }
+        content.style.left = `${count}%`;
+      }
+
+    } else if(width <= 1170) {
+      function moveLeft() {
+        if(count === 0) {
+          count = -600;
+        } else {
+          count+=120;
+        }
+        content.style.left = `${count}%`;
+      }
+  
+      function moveRight() {
+        if(count === -600) {
+          count = 0;
+        } else {
+          count-=120;
+        }
+        content.style.left = `${count}%`;
+      }
+    } else {
+      function moveLeft() {
+        if(count === 0) {
+          count = -108;
+        } else {
+          count+=36;
+        }
+        content.style.left = `${count}%`;
+      }
+  
+      function moveRight() {
+        if(count === -108) {
+          count = 0;
+        } else {
+          count-=36;
+        }
+        content.style.left = `${count}%`;
+      }
+    }
+
+
+    function moveLeft2(evt) {
+      if(evt.key === 'Enter') {
+        moveLeft();
+      }
+    }
+
+    function moveRight2(evt) {
+      if(evt.key === 'Enter') {
+        moveRight();
+      }
+    }
+
+    left.addEventListener('click', moveLeft);
+    left.addEventListener('keydown', moveLeft2);
+
+    right.addEventListener('click', moveRight);
+    right.addEventListener('keydown', moveRight2);
   }
 
   createSlider(leftArrow, rightArrow, contentBox);
